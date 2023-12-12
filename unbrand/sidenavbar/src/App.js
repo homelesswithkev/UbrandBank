786920bb39e1fdc88bf5614743c13bf699a556ef
homelesskev <153132681+homelesswithkev@users.noreply.github.com>
2023-12-12 19:05:12 +0000

    Update 11 files owwwwwwwww

--- unbrand/sidenavbar/src/App.js
+++ unbrand/sidenavbar/src/App.js
@@ -1,13 +1,8 @@
 import "./App.css";
 import React from "react";
-import SideNavBar from "./components/SideNavBar";
 
 function App() {
-  return (
-    <div className="App">
-      <SideNavBar />
-    </div>
-  );
+  return;
 }
 
 export default App;
